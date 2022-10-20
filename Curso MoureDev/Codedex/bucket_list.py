e812bd83013df6ecf77aa643ffd2f536ad2a9ce1
Marimar Beiras <marimar492@gmail.com>
2022-10-20 01:12:14 -0400

    Ejercicios de Codedex

---
+++ Curso MoureDev/Codedex/bucket_list.py
@@ -0,0 +1,21 @@
+things_to_do = [
+    'Travel to Italy, Belgium, UK, France, Hungary',
+    'Tour Spain',
+    'Meet the Love of my life',
+    'Travel with my mom',
+    'Travel with my friends',
+    'Have a puppy',
+    'Buy an apartment',
+    'Have a job that I love',
+    'Travel with my love',
+    'Have a lot of parties with my family',
+    'eat at different places',
+    'Visit Marruecos',
+    'Being able to see the northern lights ',
+    'Have an enterprise with my friends',
+    'Live in another country',
+    'Travel to Aruba an Curacao'
+]
+
+for wish in things_to_do:
+    print(wish)
---
+++ Curso MoureDev/Codedex/mixtape.py
@@ -0,0 +1,19 @@
+# New Hits
+i = 0
+playlist = [
+    'Sam Smith, Kim Petras - UNHOLY',
+    'Los Mesoneros - Despidete Bien',
+    'BANNERS - Perfectly Broken',
+    'Allison - Gracias por la Herida',
+    'Aitana - Otra vez',
+    'Beyoncé - CUFF IT ',
+    'Nani- MANTRA',
+    'Lasso - Yo-yo'
+]
+
+# for song in playlist:
+#     print(song)
+
+while i < len(playlist):
+    print(playlist[i])
+    i += 1
